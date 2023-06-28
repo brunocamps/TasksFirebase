564a845e633d0bdad4400450521e53fb1056edab
brunocamps <bruno.mcamps@outlook.com>
2023-06-27 21:07:37 -0300

    Initial Commit

---
+++ TasksFirebase/TasksFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksFirebaseApp.swift
+//  TasksFirebase
+//
+//  Created by Bruno Campos on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TasksFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
